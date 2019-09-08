17bf7beab0bd9c8581d96ff8775548b7a22a649c
test <test@teva.com>
2019-09-08 14:31:16 +0300

    Added Patroni cluster for 29-th lesson

---
+++ 29-Postgresql-Patroni/README.md
@@ -0,0 +1,5 @@
+Patroni cluster развернутый на докер хостах(Для экономия места) посредствам ansible 
+
+
+
+https://www.youtube.com/watch?v=XNPJCnqCNzg
---
+++ 29-Postgresql-Patroni/provision.sh
@@ -0,0 +1,4 @@
+cp -R /vagrant/dockerlab/ /home/vagrant/
+#sudo chown -R 1000:1000 /var/
+#docker exec -it jenkins_jenkins_1 /bin/bash
+#sudo chown -R 1000:1000 /usr
